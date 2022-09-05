70fcb093bcf3c572ae0d05feb2c3d2014d7911a5
jl-molina <joseluisml1218@gmail.com>
2022-09-04 22:03:17 -0600

    Promises, async and await class - first part

---
+++ promisesAsyncAwait/index.js
@@ -0,0 +1,46 @@
+const toNumber = n => Number(n);
+const multiply = n => n * 2;
+const print = n => console.log(n);
+
+const altThen = n => console.log(n);
+
+const catchAndPrint = err => console.log(err);
+
+const request = (url) => {
+    return new Promise((resolve, reject) => {
+        const req = new XMLHttpRequest();
+        req.open('GET', url);
+
+        req.onload = () => {
+            if (req.status === 200) {
+                resolve(req.responseText);
+            } else {
+                reject(new Error('Error al cargar'));
+            }
+        }
+
+        req.onerror = () => {
+            reject(new Error('Error de red'));
+        }
+
+        req.send();
+    })
+}
+
+// then(data => toNumber(data)) is the same as then(toNumber);
+request('./config.txt')
+    .then(data => toNumber(data))
+    .then(multiply)
+    .then(print)
+    .catch(catchAndPrint);
+request('./config1.txt')
+    .then(data => toNumber(data))
+    .then(multiply)
+    .then(print)
+    .catch(catchAndPrint);
+request('http://madeup.com')
+    .then(data => toNumber(data))
+    .then(multiply)
+    .then(print)
+    .catch(catchAndPrint);
+
